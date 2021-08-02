057f32d36e679c2c0289a5e5eaab207f4243edba
Aaryamann Singh <aaryamannsingh3@gmail.com>
2021-08-02 19:56:22 +0530

    check fix

--- src/dynamic-option-list.jsx
+++ src/dynamic-option-list.jsx
@@ -106,7 +106,7 @@ export default class DynamicOptionList extends React.Component {
                     </div> }
                     { this.props.canHaveOptionValue && this.props.canHaveOptionCorrect &&
                     <div className="col-sm-1">
-                      <input className="form-control" type="checkbox" value="1" onChange={this.editOptionCorrect.bind(this, index)} checked={option.hasOwnProperty('correct')} />
+                      <input className="form-check-input" type="checkbox" value="1" onChange={this.editOptionCorrect.bind(this, index)} checked={option.hasOwnProperty('correct')} />
                     </div> }
                     <div className="col-sm-3">
                       <div className="dynamic-options-actions-buttons">
